5c357efecc8e641494aaf5ec595670fc6b587b19
nikolalazarevic011 <nikola.lazaa@gmail.com>
2025-01-10 14:09:12 -0600

    update UpdatedDetailedComp.jsx to only show when the classes are starting without cards when there's no data

--- src/components/home and classes page/UpdatedDetailedComp.jsx
+++ src/components/home and classes page/UpdatedDetailedComp.jsx
@@ -18,6 +18,16 @@ const UpdatedDetailedComp = ({ data, loading }) => {
     const location = useLocation();
 
     const isBasePath = location.pathname === basePath;
+    const currentYear = new Date().getFullYear();
+
+    // Determine if classes are inactive
+    const noActiveClasses =
+        !data ||
+        (!data.class_title &&
+            !data.exercize_video &&
+            !data.homework_title &&
+            !data.handout_title);
+
     return (
         <>
             <Toolbar />
@@ -30,6 +40,22 @@ const UpdatedDetailedComp = ({ data, loading }) => {
                         my={3}
                     >
                         <Skeleton variant="text" width="40%" height={50} />
+                    </Box>
+                ) : noActiveClasses ? (
+                    <Box
+                        width="100%"
+                        height="50vh"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <Typography
+                            variant="h6"
+                            color="text.secondary"
+                            textAlign="center"
+                        >
+                            Classes starting in March {currentYear}
+                        </Typography>
                     </Box>
                 ) : (
                     <>
@@ -44,6 +70,7 @@ const UpdatedDetailedComp = ({ data, loading }) => {
                                 ? "Latest class information"
                                 : `${data.title} information`}
                         </Typography>
+                        {/* //! CERTIFICATE */}
                         {/* <Box display="flex" justifyContent="center">
                             <Button
                                 variant="contained"
@@ -60,126 +87,160 @@ const UpdatedDetailedComp = ({ data, loading }) => {
                         </Box> */}
                     </>
                 )}
-                <Grid container spacing={3}>
-                    {loading ? (
-                        // Show 3 placeholders while loading
-                        Array.from({ length: 3 }, (_, index) => (
-                            <Grid item xs={12} md={4} key={`loading-${index}`}>
-                                <VideoCard loading={true} />
-                            </Grid>
-                        ))
-                    ) : (
-                        // Show all 4 VideoCard components with actual data after loading is done
-                        <>
-                            <Grid item xs={12} md={4}>
-                                <VideoCard
-                                    title={data.class_title}
-                                    url={data.class_video_url}
-                                    description={data.class_week_description}
-                                    // toDetailsPage={data.class_document_1} // all class docs are now in the handout card
-                                    toPage={`${basePath}classes`}
-                                    subheader="Class Replay"
-                                    loading={false}
-                                />
-                            </Grid>
-                            <Grid item xs={12} md={4}>
-                                <VideoCard
-                                    title={data.exercise_title}
-                                    url={data.exercize_video}
-                                    description={data.exercise_week_description}
-                                    toPage={`${basePath}exercises`}
-                                    toDetailsPage={data.exercize_pdf}
-                                    subheader="Exercise of the Week"
-                                    link1Href={data.exercize_pdfs[1]}
-                                    link1Title={data.exercise_2_title}
-                                    link2Href={data.exercize_pdfs[2]}
-                                    link2Title={data.exercise_3_title || ""}
-                                    link3Href={data.exercize_pdfs[3]}
-                                    link3Title={data.handout_doc_4_title}
-                                />
-                            </Grid>
-                            {/* Third and Fourth VideoCard components are placed in a nested Grid container */}
-                            <Grid item xs={12} md={4}>
-                                <Grid container spacing={3} direction="column">
-                                    <Grid item xs={12}>
-                                        {!classId ? (
+                {!noActiveClasses && (
+                    <Grid container spacing={3}>
+                        {loading ? (
+                            // Show 3 placeholders while loading
+                            Array.from({ length: 3 }, (_, index) => (
+                                <Grid
+                                    item
+                                    xs={12}
+                                    md={4}
+                                    key={`loading-${index}`}
+                                >
+                                    <VideoCard loading={true} />
+                                </Grid>
+                            ))
+                        ) : (
+                            // Show all 4 VideoCard components with actual data after loading is done
+                            <>
+                                <Grid item xs={12} md={4}>
+                                    <VideoCard
+                                        title={data.class_title}
+                                        url={data.class_video_url}
+                                        description={
+                                            data.class_week_description
+                                        }
+                                        // toDetailsPage={data.class_document_1} // all class docs are now in the handout card
+                                        toPage={`${basePath}classes`}
+                                        subheader="Class Replay"
+                                        loading={false}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <VideoCard
+                                        title={data.exercise_title}
+                                        url={data.exercize_video}
+                                        description={
+                                            data.exercise_week_description
+                                        }
+                                        toPage={`${basePath}exercises`}
+                                        toDetailsPage={data.exercize_pdf}
+                                        subheader="Exercise of the Week"
+                                        link1Href={
+                                            data.exercize_pdfs &&
+                                            data.exercize_pdfs[1]
+                                        }
+                                        link1Title={data.exercise_2_title}
+                                        link2Href={
+                                            data.exercize_pdfs &&
+                                            data.exercize_pdfs[2]
+                                        }
+                                        link2Title={data.exercise_3_title || ""}
+                                        link3Href={
+                                            data.exercize_pdfs &&
+                                            data.exercize_pdfs[3]
+                                        }
+                                        link3Title={data.handout_doc_4_title}
+                                    />
+                                </Grid>
+                                {/* Third and Fourth VideoCard components are placed in a nested Grid container */}
+                                <Grid item xs={12} md={4}>
+                                    <Grid
+                                        container
+                                        spacing={3}
+                                        direction="column"
+                                    >
+                                        <Grid item xs={12}>
+                                            {!classId ? (
+                                                <VideoCard
+                                                    title={
+                                                        data.homework_title ||
+                                                        ""
+                                                    }
+                                                    // url={data.class_video_url || ""}
+                                                    description={
+                                                        data.homework_week_description ||
+                                                        ""
+                                                    }
+                                                    // toPage={`${basePath}homework`}
+                                                    toDetailsPage={
+                                                        data.homework_pdf || ""
+                                                    }
+                                                    subheader={
+                                                        "Homework of the week"
+                                                    }
+                                                    loading={false}
+                                                />
+                                            ) : (
+                                                <VideoCard
+                                                    title={
+                                                        data.homework_title ||
+                                                        ""
+                                                    }
+                                                    // url={data.class_video_url || ""}
+                                                    description={
+                                                        classId !==
+                                                        data.newestClass
+                                                            ? "No longer available"
+                                                            : data.homework_week_description ||
+                                                              ""
+                                                    }
+                                                    // toPage={`${basePath}homework`}
+                                                    toDetailsPage={
+                                                        classId !==
+                                                        data.newestClass
+                                                            ? null
+                                                            : data.homework_pdf
+                                                    }
+                                                    subheader={
+                                                        "Homework of the week"
+                                                    }
+                                                    loading={false}
+                                                />
+                                            )}
+                                        </Grid>
+                                        <Grid item xs={12} md={3}>
                                             <VideoCard
-                                                title={
-                                                    data.homework_title || ""
-                                                }
-                                                // url={data.class_video_url || ""}
+                                                title={data.handout_title || ""}
                                                 description={
-                                                    data.homework_week_description ||
+                                                    data.handout_week_description ||
                                                     ""
                                                 }
-                                                // toPage={`${basePath}homework`}
+                                                toPage={`${basePath}handouts`}
                                                 toDetailsPage={
-                                                    data.homework_pdf || ""
+                                                    data.class_document_1
+                                                }
+                                                link1Href={
+                                                    data.class_document_2
+                                                }
+                                                link1Title={
+                                                    data.handout_doc_2_title
+                                                }
+                                                link2Href={
+                                                    data.class_document_3
+                                                }
+                                                link2Title={
+                                                    data.handout_doc_3_title
+                                                }
+                                                link3Href={
+                                                    data.class_document_4
+                                                }
+                                                link3Title={
+                                                    data.handout_doc_4_title
                                                 }
                                                 subheader={
-                                                    "Homework of the week"
+                                                    "Handouts or/and weekly challenges"
                                                 }
                                                 loading={false}
                                             />
-                                        ) : (
-                                            <VideoCard
-                                                title={
-                                                    data.homework_title || ""
-                                                }
-                                                // url={data.class_video_url || ""}
-                                                description={
-                                                    classId !== data.newestClass
-                                                        ? "No longer available"
-                                                        : data.homework_week_description ||
-                                                          ""
-                                                }
-                                                // toPage={`${basePath}homework`}
-                                                toDetailsPage={
-                                                    classId !== data.newestClass
-                                                        ? null
-                                                        : data.homework_pdf
-                                                }
-                                                subheader={
-                                                    "Homework of the week"
-                                                }
-                                                loading={false}
-                                            />
-                                        )}
-                                    </Grid>
-                                    <Grid item xs={12} md={3}>
-                                        <VideoCard
-                                            title={data.handout_title || ""}
-                                            description={
-                                                data.handout_week_description ||
-                                                ""
-                                            }
-                                            toPage={`${basePath}handouts`}
-                                            toDetailsPage={
-                                                data.class_document_1
-                                            }
-                                            link1Href={data.class_document_2}
-                                            link1Title={
-                                                data.handout_doc_2_title
-                                            }
-                                            link2Href={data.class_document_3}
-                                            link2Title={
-                                                data.handout_doc_3_title
-                                            }
-                                            link3Href={data.class_document_4}
-                                            link3Title={
-                                                data.handout_doc_4_title
-                                            }
-                                            subheader={
-                                                "Handouts or/and weekly challenges"
-                                            }
-                                            loading={false}
-                                        />
+                                        </Grid>
                                     </Grid>
                                 </Grid>
-                            </Grid>
-                        </>
-                    )}
-                </Grid>
+                            </>
+                        )}
+                    </Grid>
+                )}
             </Container>
             <Toolbar />
         </>
